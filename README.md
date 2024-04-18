a6469514c48def08353419dd96518e6bcf81cc23
Cmellons <codymellons@yahoo.com>
2024-04-18 17:04:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# to-do-list
+ React and Redux project
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+
+
+  return (
+      <div className="text-5xl font-bold">
+        home component
+      </div>
+  )
+}
+
+export default App
